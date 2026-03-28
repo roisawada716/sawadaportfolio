e953bcd1809c5c4ea7ad7c876bb31aa731b012ce
roisawada716 <alvendia.baste@gmail.com>
2026-03-28 13:53:49 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+});
+
